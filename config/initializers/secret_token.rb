bfbb38ba050ab319d4d45af398cbf3068ab1cea1
maxsystemtech <ricardo.eights@maxsystemtech.com>
2013-12-08 09:41:52 -0400

    Added rails folder structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hoowenware::Application.config.secret_key_base = '969e8ef51d261bbd7a013a0b7d19ec31994c837479d2e824ed9ab65a86097435c5f9ed706a1ec1ee73bcf6b2a7ba0daf5c1dfeaf316010b7f6d92acca761ad61'
